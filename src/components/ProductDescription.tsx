122cf60204aedca582449a1651d002ae02882ae6
horiaomar25 <horiaomar6@gmail.com>
2024-09-08 17:56:26 +0100

    update to productpage styling

--- src/components/ProductDescription.tsx
+++ src/components/ProductDescription.tsx
@@ -1,102 +1,114 @@
-import React from 'react'
+import React, { useState } from 'react';
 import { Link } from "react-router-dom";
 import { Product } from '../types/Products';
 
-
 interface ProductDetailProps {
-    product: Product;
-  }
+  product: Product;
+}
 
-const ProductDescription:React.FC<ProductDetailProps> = ({ product }) => {
+const ProductDescription: React.FC<ProductDetailProps> = ({ product }) => {
+  const [quantity, setQuantity] = useState(0);
+
+  const handleIncrease = () => {
+    setQuantity(quantity + 1);
+  };
+
+  const handleDecrease = () => {
+    if (quantity > 0) {
+      setQuantity(quantity - 1);
+    }
+  };
+
   return (
     <>
-     {/* Product Image */}
-     <div className="mt-5">
-     <figure className="p-4">
-       <div className="w-full h-auto overflow-hidden">
-         <img
-           src={product.thumbnail}
-           alt={product.title}
-           width={500}
-           height={500}
-         />
-       </div>
-     </figure>
-   </div>
+      {/* Product Image */}
+      <div className="mt-5 mb-8">
+        <figure className="p-4">
+          <div className="w-full h-auto overflow-hidden">
+            <img
+              src={product.thumbnail}
+              alt={product.title}
+              width={500}
+              height={500}
+             
+            />
+          </div>
+        </figure>
+      </div>
 
-   {/* Product Details */}
-   <div className="m-4">
-     <h3 className="text-lg font-bold mb-4">{product.title}</h3>
-     <p className="font-bold text-xl mb-4">£{product.price}</p>
-     <p className="mb-4">{product.description}</p>
+      {/* Product Details */}
+      <div className="m-4 mt-10">
+        <h3 className="text-2xl font-bold mb-6">{product.title}</h3>
+        <p className="font-bold text-xl mb-6">£{product.price}</p>
+        <p className="mb-6">{product.description}</p>
 
-     <Link to="/productlist">
-       <button className="border border-black text-black py-2 px-4 rounded-lg w-full text-center mt-3 hover:bg-black hover:text-white lg:w-1/4">
-         Add to cart
-       </button>
-     </Link>
+        <div className="flex items-center xs:justify-center   space-x-2 mb-6">
+          <button
+            onClick={handleDecrease}
+            className="border border-black text-black py-2 px-4 rounded-lg  hover:bg-black hover:text-white"
+          >
+            -
+          </button>
+          <input
+            type="text"
+            className="w-20 h-10 text-center border border-black-300 rounded-lg py-1"
+            value={quantity}
+            readOnly
+          />
+          <button
+            onClick={handleIncrease}
+            className="border border-black text-black py-2 px-4 rounded-lg hover:bg-black hover:text-white"
+          >
+            +
+          </button>
+        </div>
 
-     <div className="flex items-center space-x-2 mt-4">
-       <button
-         id="decrease"
-         className="bg-gray-200 text-gray-800 hover:bg-gray-300 p-2 rounded border border-gray-400"
-       >
-         -
-       </button>
-       <input
-         type="text"
-         id="quantity"
-         className="w-12 text-center border border-gray-300 rounded py-1"
-         value="0"
-       />
-       <button
-         id="increase"
-         className="bg-gray-200 text-gray-800 hover:bg-gray-300 p-2 rounded border border-gray-400"
-       >
-         +
-       </button>
-     </div>
+        <Link to="/productlist">
+          <button className="border border-black text-black py-2 px-4 rounded-lg w-full  lg:w-11/12 text-center mt-3 hover:bg-black hover:text-white ">
+            Add to cart
+          </button>
+        </Link>
 
-     {/* Product Rating */}
-     <><div className="mt-4 flex items-center">
-    {[...Array(Math.round(product.rating))].map((_, i) => (
-        <svg
-            key={i}
-            className="w-4 h-4 text-yellow-500"
-            fill="currentColor"
-            viewBox="0 0 24 24"
-        >
-            <path d="M12 17.27L18.18 21 16.54 14.53 22 9.27H15.81L12 2 8.19 9.27H2L7.46 14.53 5.82 21 12 17.27z" />
-        </svg>
-    ))}
-    {Array(5 - Math.round(product.rating))
-        .fill(null)
-        .map((_, i) => (
+        {/* Product Rating */}
+        <div className="lg:mt-6 mb-4 mt-4 flex items-center">
+          {[...Array(Math.round(product.rating))].map((_, i) => (
             <svg
+              key={i}
+              className="w-4 h-4 text-yellow-500"
+              fill="currentColor"
+              viewBox="0 0 24 24"
+            >
+              <path d="M12 17.27L18.18 21 16.54 14.53 22 9.27H15.81L12 2 8.19 9.27H2L7.46 14.53 5.82 21 12 17.27z" />
+            </svg>
+          ))}
+          {Array(5 - Math.round(product.rating))
+            .fill(null)
+            .map((_, i) => (
+              <svg
                 key={i + Math.round(product.rating)}
                 className="w-4 h-4 text-gray-300"
                 fill="currentColor"
                 viewBox="0 0 24 24"
-            >
+              >
                 <path d="M12 17.27L18.18 21 16.54 14.53 22 9.27H15.81L12 2 8.19 9.27H2L7.46 14.53 5.82 21 12 17.27z" />
-            </svg>
-        ))}
-    <p className="mr-2">{product.rating}</p>
-</div><div className="mt-4 flex flex-wrap">
-        {product.tags.map((tag, index) => (
+              </svg>
+            ))}
+          <p className="ml-2">{product.rating}</p>
+        </div>
+
+        <div className="lg:mt-28 flex flex-wrap ">
+          {product.tags.map((tag, index) => (
             <span
-                key={index}
-                className="bg-gray-100 text-gray-700 px-2 py-1 rounded mr-2 mb-2"
+              key={index}
+              className="bg-gray-100 text-gray-700 px-2 py-1 rounded mr-2 mb-2"
             >
-                {tag}
+              {tag}
             </span>
-        ))}
-    </div></>
+          ))}
+        </div>
+      </div>
+    </>
+  );
+};
 
-     
-   </div>
-   </>
-  )
-}
-
-export default ProductDescription
+export default ProductDescription;
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,7 @@ export default {
       center: true,
       padding: "2rem",
       screens: {
+        xs: "390px",
         sm: "640px",
         md: "768px",
         lg: "1024px",
