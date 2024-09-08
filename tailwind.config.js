6ecaab6e1c69d286b934369429524a14a1017107
horiaomar25 <horiaomar6@gmail.com>
2024-09-08 15:23:26 +0100

    intial commit

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+import daisyui from "daisyui";
+
+export default {
+  content: [
+    "./pages/**/*.{js,jsx,ts,tsx}",
+    "./components/**/*.{js,jsx,ts,tsx}",
+    "./app/**/*.{js,jsx,ts,tsx}",
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        sm: "640px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
+      },
+    },
+  },
+  daisyui: {
+    themes: [],
+  },
+  plugins: [daisyui],
+};
