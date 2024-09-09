e3c85f42baebf0e43537e00370e887c62e949d93
horiaomar25 <horiaomar6@gmail.com>
2024-09-09 19:45:56 +0100

    added: checkout page added

--- src/components/Payment.tsx
+++ src/components/Payment.tsx
@@ -1,9 +1,54 @@
-
-
 const Payment = () => {
   return (
-    <div>Payment</div>
-  )
-}
+    <>
+      <section className=" p-10">
+        <form className="mb-4">
+          <label className="input input-bordered flex items-center gap-2 mb-4">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 16 16"
+              fill="currentColor"
+              className="h-4 w-4 opacity-70"
+            >
+              <path
+                d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z"
+              />
+            </svg>
+            <input type="text" className="grow" placeholder="Name" />
+            
+            
+          </label>
+          <label className="input input-bordered flex items-center gap-2 mb-4">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 16 16"
+              fill="currentColor"
+              className="h-4 w-4 opacity-70"
+            >
+              <path
+                d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z"
+              />
+              <path
+                d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z"
+              />
+            </svg>
+            <input type="text" className="grow" placeholder="Email" />
+          </label>
+          <label className="input input-bordered flex items-center gap-2 mb-4">
+           
+            <input type="text" className="grow" placeholder="Account Number" />
+          </label>
+          <label className="input input-bordered flex items-center gap-2 mb-4">
+           
+            <input type="text" className="grow"  />
+          </label>
+          <button className="mt-4 border border-black text-black py-2 px-4 rounded-lg hover:bg-black hover:text-white w-full">
+                Proceed to Payment
+              </button>
+        </form>
+      </section>
+    </>
+  );
+};
 
-export default Payment
+export default Payment;
--- src/routes/Checkout.tsx
+++ src/routes/Checkout.tsx
@@ -1,4 +1,5 @@
 import useCart from "../hooks/useCart";
+import Payment from "../components/Payment";
 
 const CheckOut = () => {
   const { cartItems } = useCart(); // Extract cartItems from useCart
@@ -6,47 +7,56 @@ const CheckOut = () => {
   console.log(cartItems); // Add this line to check if cartItems has data
 
   return (
-    <div className="container mx-auto py-8">
-      <h1 className="text-4xl font-bold mb-4">Checkout</h1>
-      {cartItems.length === 0 ? (
-        <p>Your cart is empty.</p>
-      ) : (
-        <div>
-          <h2 className="text-2xl font-semibold mb-4 text-black">Your Cart Items</h2>
-          <ul className="list-disc pl-5">
-            {cartItems.map((item) => (
-              <li key={item.id} className="mb-4">
-                <div className="flex items-center justify-between">
-                  <div className="flex items-center">
-                    <img
-                      src={item.image}
-                      alt={item.title}
-                      className="w-16 h-16 object-contain mr-4"
-                    />
-                    <div>
-                      <p className="font-bold">{item.title}</p>
-                      <p>Price: £{item.price.toFixed(2)}</p>
-                      <p>Quantity: {item.quantity}</p>
+    <>
+      <section className="grid grid-cols-1 lg:grid-cols-2 gap-4 justify-center m-8 p-12">
+        <div className="lg:col-span-1  p-10 ">
+          <h1 className="text-4xl font-bold mb-4">Checkout</h1>
+          {cartItems.length === 0 ? (
+            <p>Your cart is empty.</p>
+          ) : (
+            <div>
+              <h2 className="text-2xl font-semibold mb-4 text-black">
+                Your Cart Items
+              </h2>
+              <ul className="list-disc pl-5">
+                {cartItems.map((item) => (
+                  <li key={item.id} className="mb-4 list-none ">
+                    <div className="flex items-center justify-between">
+                      <div className="flex items-center">
+                        <img
+                          src={item.image}
+                          alt={item.title}
+                          className="w-16 h-16 object-contain mr-4"
+                        />
+                        <div>
+                          <p className="font-bold">{item.title}</p>
+                          <p>Price: £{item.price.toFixed(2)}</p>
+                          <p>Quantity: {item.quantity}</p>
+                        </div>
+                      </div>
                     </div>
-                  </div>
-                </div>
-              </li>
-            ))}
-          </ul>
-          <div className="mt-6">
-            <h3 className="text-xl font-semibold">Total:</h3>
-            <p className="text-lg font-bold">
-              £{cartItems.reduce((acc, item) => acc + item.price * item.quantity, 0).toFixed(2)}
-            </p>
-          </div>
-          <button className="mt-4 border border-black text-black py-2 px-4 rounded-lg hover:bg-black hover:text-white">
-            Proceed to Payment
-          </button>
+                  </li>
+                ))}
+              </ul>
+              <div className="mt-6">
+                <h3 className="text-xl font-semibold">Total:</h3>
+                <p className="text-lg font-bold">
+                  £
+                  {cartItems
+                    .reduce((acc, item) => acc + item.price * item.quantity, 0)
+                    .toFixed(2)}
+                </p>
+              </div>
+             
+            </div>
+          )}
         </div>
-      )}
-    </div>
+        <div className="lg:col-span-1">
+          <Payment />
+        </div>
+      </section>
+    </>
   );
 };
 
 export default CheckOut;
-
